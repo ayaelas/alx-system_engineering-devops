e490c33b8244e8eb9d1ee76b592230186660d71f
ayaelas <assalharras@gmail.com>
2024-02-28 06:11:54 -0800

    r

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,17 @@
-#!/usr/bin/pup
-#install an especific version of pip
+# install Flask v2.1.0  using pip3
+
+package { 'python3-pip':
+  ensure => installed,
+}
+
 package { 'flask':
   ensure   => '2.1.0',
   provider => 'pip3',
+  require  => Package['python3-pip'],
+}
+
+package { 'werkzeug':
+  ensure   => '2.1.1',
+  provider => 'pip',
+  require  => Package['python3-pip'],
 }
