7059345b17857d0fdca724d93bbd7638a5db97ba
ayaelas <assalharras@gmail.com>
2024-02-28 06:08:14 -0800

    r

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,6 @@
 #!/usr/bin/pup
 #install an especific version of pip
-package { "flask":
-  ensure   => "2.1.0",
-  provider => "pip3",
+package { 'flask':
+  ensure    =>  '2.1.0',
+  provider  =>  'pip3',
 }
