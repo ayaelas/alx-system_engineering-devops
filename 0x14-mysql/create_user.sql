3038850b9c10883302503be80f67b3f1191aa98b
ayaelas <assalharras@gmail.com>
2024-03-19 17:18:41 -0700

    script fx

--- 0x14-mysql/create_user.sql
+++
@@ -1,9 +0,0 @@
--- Creates a user
--- Responsible for checking the configuration of the servers
-
-CREATE USER IF NOT EXISTS 'holberton_user'@'localhost'
-IDENTIFIED BY 'projectcorrection280hbtn';
-
-GRANT REPLICATION CLIENT, SELECT ON *.* TO 'holberton_user'@'localhost';
-
-FLUSH PRIVILEGES;
