3561ca47cda495383eaf05f053988e7abed4f47f
ayaelas <assalharras@gmail.com>
2024-04-09 19:10:41 -0700

    api"

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -12,12 +12,11 @@ def number_of_subscribers(subreddit):
     """
     url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
     headers = {
-        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/Curious_Salt3631)"
+        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
     }
     response = requests.get(url, headers=headers,
                             allow_redirects=False)
-    if response.status_code == 200:
-        data = response.json()
-        return data.get('data').get('subscribers')
-    else:
+    if response.status_code == 404:
         return 0
+    result = response.json().get('data')
+    return result.get("subscribers")
