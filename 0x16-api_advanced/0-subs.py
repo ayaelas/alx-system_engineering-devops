937974d09a9753038577d042a199f80ead9dedbd
ayaelas <assalharras@gmail.com>
2024-04-09 19:33:05 -0700

    api

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,22 +1,21 @@
 #!/usr/bin/python3
 """
-Queries the Reddit API
-returns the number of subscribers of a subreddit
+Module that implements the number_of_subscribers function
 """
-import requests
 
 
 def number_of_subscribers(subreddit):
     """
-    return the number of subscribers
+    Queries the Reddit API,
+    Returns the number of subscribers for the given subreddit
     """
-    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
-    headers = {
-        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
-    }
-    response = requests.get(url, headers=headers,
-                            allow_redirects=False)
-    if response.status_code == 404:
+    import requests
+
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {"User-Agent": "My-User-Agent"}
+    response = requests.get(url, headers=headers, allow_redirects=False)
+
+    if response.status_code != 200:
         return 0
-    result = response.json().get('data')
-    return result.get("subscribers")
+
+    return response.json().get("data").get("subscribers")
