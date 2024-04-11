25bc117c49e9f1dae37e0e0ad3937630a8af57f5
ayaelas <assalharras@gmail.com>
2024-04-11 09:26:34 -0700

    reddir

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,24 +1,21 @@
 #!/usr/bin/python3
 """
-Returns number_of_subscribers function
+Returns the number_of_subscribers function
 """
 
-import requests
 
 def number_of_subscribers(subreddit):
-    """return nmber of subscribers given"""
-    base_url = "https://www.reddit.com/r/{}/about.json"
-    
-    
-    headers = {
-        "User-Agent": "MySubredditInfoApp/1.0 (by /u/ayaelas)"
-    }
-    
-    url = base_url.format(subreddit)
+    """
+    Queries the Rdt API,
+    Returns the number of subscribers for the given subreddit
+    """
+    import requests
+
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {"User-Agent": "My-User-Agent"}
     response = requests.get(url, headers=headers, allow_redirects=False)
-        
-    if response.status_code == 200:
-       data = response.json()
-       subscribers = data['data']['subscribers']
-       return subscribers
-    return 0
+
+    if response.status_code != 200:
+        return 0
+
+    return response.json().get("data").get("subscribers")
